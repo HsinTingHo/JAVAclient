d9455e3d97062bf54445a97f40590ceee3866480
hsintingho <yama762682@gmail.com>
2019-06-04 11:09:29 -0400

    Set proxy

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,12 @@
+import java.rmi.RemoteException; 
+import LKQIntegrationService.LKQCorp.OrderingSvcProxy;
 
 public class Client {
-
+	LKQIntegrationService.LKQCorp.OrderingSvcProxy proxy = new LKQIntegrationService.LKQCorp.OrderingSvcProxy();
+	try {
+		proxy.checkDropShipAvailability(request);
+	}
+	catch(RemoteException e){
+		e.printStackTrace();  
+	}
 }
